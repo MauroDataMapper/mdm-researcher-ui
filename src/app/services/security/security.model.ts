58b1c9562971003576c08b857fe26f70dad70ea4
Aaron Forshaw <aaron@janda.org.uk>
2021-06-11 19:00:38 +0100

    Use latest verison of mdm-resources and security types therein

--- src/app/services/security/security.model.ts
+++ src/app/services/security/security.model.ts
@@ -17,30 +17,6 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { MdmResourcesError, MdmResourcesResponse } from "@mdm/services/mdm-resources/mdm-resources.model";
 
-/**
- * Credentials to send to `mdm-resources` for the sign-in operation.
- */
-export interface SignInCredentials {
-  username: string;
-  password: string;
-}
-
-/**
- * The result of a successful sign-in operation.
- */
-export interface SignInResult {
-  id: string;
-  token?: string;
-  emailAddress: string;
-  firstName: string;
-  lastName: string;
-  pending: boolean;
-  disabled: boolean;
-  createdBy: string;
-  userRole?: string;
-  needsToResetPassword?: boolean;
-}
-
 export enum SignInErrorType {
   UnknownError,
   InvalidCredentials,
@@ -91,12 +67,6 @@ export interface AuthenticatedSessionResult {
   authenticatedSession: boolean
 }
 
-export interface AdministrationSessionResult {
-  applicationAdministrationSession: boolean;
-}
-
-export type SignInResponse = MdmResourcesResponse<SignInResult>;
-export type AdministrationSessionResponse = MdmResourcesResponse<AdministrationSessionResult>;
 export type AuthenticatedSessionResponse = MdmResourcesResponse<AuthenticatedSessionResult>
 
 /**
