c5bf3211fbc352e310f587fa20326b8bc777953b
Cristian Gimenez <c.gimenez.azcurra@oxfordcc.co.uk>
2021-07-13 15:46:32 +0100

    Make the wishlist download element more generic

---
+++ src/app/models/data-element-fields.spec.ts
@@ -0,0 +1,7 @@
+import { DataElementFields } from './data-element-fields';
+
+describe('DataElementFields', () => {
+  it('should create an instance', () => {
+    expect(new DataElementFields()).toBeTruthy();
+  });
+});
